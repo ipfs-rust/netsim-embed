01e1f9ce57ad44153c7c2116b202e8bad80fbeff
David Craven <david@craven.ch>
2021-05-18 17:46:20 +0200

    Remove unwrapping.

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -23,7 +23,7 @@ use futures::sink::SinkExt;
 use futures::stream::StreamExt;
 use netsim_embed_core::{Ipv4Range, Packet, Plug};
 use std::fmt::Display;
-use std::io::Write;
+use std::io::{Error, ErrorKind, Result, Write};
 use std::net::Ipv4Addr;
 use std::process::Stdio;
 use std::str::FromStr;
@@ -47,42 +47,43 @@ pub fn machine<C, E>(
     ns_tx: oneshot::Sender<Namespace>,
     mut cmd: mpsc::UnboundedReceiver<C>,
     event: mpsc::UnboundedSender<E>,
-) -> thread::JoinHandle<()>
+) -> thread::JoinHandle<Result<()>>
 where
     C: Display + Send + 'static,
     E: FromStr + Send + 'static,
-    E::Err: std::fmt::Debug + Send,
+    E::Err: std::fmt::Debug + std::error::Error + Send + Sync,
 {
     thread::spawn(move || {
         tracing::trace!("spawning machine with addr {}", addr);
-        let ns = Namespace::unshare().unwrap();
-        ns_tx.send(ns).unwrap();
+        let ns = Namespace::unshare()?;
+        let _ = ns_tx.send(ns);
 
         async_global_executor::block_on(async move {
-            let iface = iface::Iface::new().unwrap();
-            iface.set_ipv4_addr(addr, mask).unwrap();
+            let iface = iface::Iface::new()?;
+            iface.set_ipv4_addr(addr, mask)?;
             iface.put_up().unwrap();
-            iface.add_ipv4_route(Ipv4Range::global().into()).unwrap();
+            iface.add_ipv4_route(Ipv4Range::global().into())?;
 
-            let iface = async_io::Async::new(iface).unwrap();
+            let iface = async_io::Async::new(iface)?;
             let (mut tx, mut rx) = plug.split();
 
             let ctrl_task = async {
                 while let Some(ctrl) = ctrl.next().await {
                     match ctrl {
-                        IfaceCtrl::Up => iface.get_ref().put_up().unwrap(),
-                        IfaceCtrl::Down => iface.get_ref().put_down().unwrap(),
+                        IfaceCtrl::Up => iface.get_ref().put_up()?,
+                        IfaceCtrl::Down => iface.get_ref().put_down()?,
                         IfaceCtrl::SetAddr(addr, mask) => {
-                            iface.get_ref().set_ipv4_addr(addr, mask).unwrap();
+                            iface.get_ref().set_ipv4_addr(addr, mask)?;
                         }
                     }
                 }
+                Result::Ok(())
             };
 
             let reader_task = async {
                 loop {
                     let mut buf = [0; libc::ETH_FRAME_LEN as usize];
-                    let n = iface.read_with(|iface| iface.recv(&mut buf)).await.unwrap();
+                    let n = iface.read_with(|iface| iface.recv(&mut buf)).await?;
                     if n == 0 {
                         break;
                     }
@@ -95,10 +96,11 @@ where
                     if let Some(mut packet) = Packet::new(&mut bytes) {
                         packet.set_checksum();
                     }
-                    if tx.send(bytes).await.is_err() {
+                    if let Err(_) = tx.send(bytes).await {
                         break;
                     }
                 }
+                Result::Ok(())
             };
 
             let writer_task = async {
@@ -111,10 +113,11 @@ where
                         }
                     }
                 }
+                Result::Ok(())
             };
 
             bin.stdin(Stdio::piped()).stdout(Stdio::piped());
-            let mut child = bin.spawn().unwrap();
+            let mut child = bin.spawn()?;
             let mut stdout = BufReader::new(child.stdout.take().unwrap()).lines().fuse();
             let mut stdin = child.stdin.unwrap();
 
@@ -123,23 +126,31 @@ where
                 while let Some(cmd) = cmd.next().await {
                     buf.clear();
                     tracing::trace!("{}", cmd);
-                    writeln!(buf, "{}", cmd).unwrap();
-                    stdin.write_all(&buf).await.unwrap();
+                    writeln!(buf, "{}", cmd)?;
+                    stdin.write_all(&buf).await?;
                 }
+                Result::Ok(())
             };
 
             let event_task = async {
                 while let Some(ev) = stdout.next().await {
-                    let ev = ev.unwrap();
+                    let ev = ev?;
                     if ev.starts_with('<') {
-                        event.unbounded_send(ev.parse().unwrap()).unwrap();
+                        let ev = match ev.parse() {
+                            Ok(ev) => ev,
+                            Err(err) => return Err(Error::new(ErrorKind::Other, err)),
+                        };
+                        if let Err(_) = event.unbounded_send(ev) {
+                            break;
+                        }
                     } else {
                         println!("{}", ev);
                     }
                 }
+                Result::Ok(())
             };
 
-            futures::future::join5(
+            let (res1, res2, res3, res4, res5) = futures::future::join5(
                 ctrl_task,
                 reader_task,
                 writer_task,
@@ -147,6 +158,12 @@ where
                 event_task,
             )
             .await;
+            res1?;
+            res2?;
+            res3?;
+            res4?;
+            res5?;
+            Ok(())
         })
     })
 }
