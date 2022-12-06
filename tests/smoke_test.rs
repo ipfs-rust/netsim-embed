be120864807bd617d5070e66609a850531c6c03f
Léo Gaspard <leo@gaspard.io>
2022-12-06 22:39:40 +0100

    do not rely on linker shenanigans to work with a single binary

--- tests/smoke_test.rs
+++ tests/smoke_test.rs
@@ -1,14 +1,12 @@
 use ipc_channel::ipc::{IpcReceiver, IpcSender};
 
-#[no_mangle]
-pub fn send_one(r: IpcReceiver<IpcSender<usize>>) {
-    let sender = r.recv().unwrap();
+#[netsim_embed::machine]
+fn send_one(sender: IpcSender<usize>) {
     sender.send(1).unwrap();
 }
 
-#[no_mangle]
-pub fn add(r: IpcReceiver<(IpcReceiver<usize>, IpcReceiver<usize>, IpcSender<usize>)>) {
-    let (left, right, sender) = r.recv().unwrap();
+#[netsim_embed::machine]
+fn add((left, right, sender): (IpcReceiver<usize>, IpcReceiver<usize>, IpcSender<usize>)) {
     sender
         .send(left.recv().unwrap() + right.recv().unwrap())
         .unwrap();
@@ -17,8 +15,7 @@ pub fn add(r: IpcReceiver<(IpcReceiver<usize>, IpcReceiver<usize>, IpcSender<usi
 fn can_send_one() {
     let mut s = netsim_embed::Netsim::<String, String>::new();
     let (sender, receiver) = ipc_channel::ipc::channel().unwrap();
-    let (_, mach_sender) = async_std::task::block_on(s.spawn(send_one));
-    mach_sender.send(sender).unwrap();
+    let _ = async_std::task::block_on(s.spawn(send_one, sender));
     assert_eq!(1, receiver.recv().unwrap());
 }
 
@@ -27,16 +24,13 @@ fn one_plus_one_makes_two() {
         let mut s = netsim_embed::Netsim::<String, String>::new();
 
         let (sender1, receiver1) = ipc_channel::ipc::channel::<usize>().unwrap();
-        let (_, mach_sender1) = s.spawn(send_one).await;
-        mach_sender1.send(sender1).unwrap();
+        let _ = s.spawn(send_one, sender1).await;
 
         let (sender2, receiver2) = ipc_channel::ipc::channel::<usize>().unwrap();
-        let (_, mach_sender2) = s.spawn(send_one).await;
-        mach_sender2.send(sender2).unwrap();
+        let _ = s.spawn(send_one, sender2).await;
 
         let (sender3, receiver3) = ipc_channel::ipc::channel::<usize>().unwrap();
-        let (_, mach_sender3) = s.spawn(add).await;
-        mach_sender3.send((receiver1, receiver2, sender3)).unwrap();
+        let _ = s.spawn(add, (receiver1, receiver2, sender3)).await;
 
         assert_eq!(2, receiver3.recv().unwrap());
     })
